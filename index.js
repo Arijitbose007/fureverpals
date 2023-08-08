f7a2637ba07ad3ae0ed8d8f6b6857e67b84dc4cd
Arijit Bose <arijitbose0007@gmail.com>
2023-08-08 14:42:16 +0530

    commit 5

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
 // Script for to get Github contributors list
-const contributors = document.getElementById("contributors");
-const repo = "PetMe";
-const owner = "akshitagupta15june";
-const apiURL = `https://api.github.com/repos/${owner}/${repo}/contributors`;
-
-
-function displayContributors(contributorsList) {
-  contributorsList.forEach((contributor) => {
-    // create anchor tag and set relevant attributes
-    const link = document.createElement("a");
-    link.setAttribute("href", contributor.html_url);
-    link.setAttribute("target", "_blank");
-
-    // create image element and set relevant attributes
-    const avatar = document.createElement("img");
-    avatar.setAttribute("class", "avatar");
-    avatar.setAttribute("src", contributor.avatar_url);
-    avatar.setAttribute("title", contributor.login);
-    avatar.setAttribute("alt", contributor.login);
-
-    link.appendChild(avatar);
-    contributors.appendChild(link);
-  });
-}
 
 function hideBackToTopButton() {
   const bttButton = document.getElementById("bttbutton");
